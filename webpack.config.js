410065ae63a6e6bcc62aa7e8048dddd6147f0be7
Alexander Demin <bouvens@gmail.com>
2018-12-23 23:02:32 +0600

    fix: Repair production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
-const demoPath = path.join(__dirname, 'demo/')
-const devMode = process.env.NODE_ENV !== 'production'
+module.exports = (env, argv) => {
+  const demoPath = path.join(__dirname, 'demo/')
+  const devMode = argv.mode !== 'production'
 
-module.exports = {
-  entry: [
-    path.join(demoPath, 'index.js'),
-    path.join(demoPath, 'style.css'),
-  ],
-  output: {
-    path: path.join(__dirname, './build'),
-    publicPath: devMode ? '/' : 'number-to-color/',
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+  return {
+    entry: [
+      path.join(demoPath, 'index.js'),
+      path.join(demoPath, 'style.css'),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(demoPath, 'index.html'),
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+    output: {
+      path: path.join(__dirname, './build'),
+      publicPath: devMode ? '/' : '/number-to-color/',
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(demoPath, 'index.html'),
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
+  }
 }
