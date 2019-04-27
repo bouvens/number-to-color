4f185f99a8557255514b944f5fb4e24a328c0900
Alexander Demin <bouvens@gmail.com>
2019-04-28 00:32:35 +0600

    feat: Optimize getColor, add benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const benchmark = require('benchmark')
+const getColor = require('../index')
+const getColorNew = require('./getColor')
+
+const states = 100
+
+const repeatForAllNumbers = (func) => {
+  for (let i = 0; i < states; i += 1) {
+    func(i, states)
+  }
+}
+const suite = new benchmark.Suite()
+
+suite
+  .add('new', () => {
+    repeatForAllNumbers(getColorNew)
+  })
+  .add('old', () => {
+    repeatForAllNumbers(getColor)
+  })
+  .add('repeat new', () => {
+    repeatForAllNumbers(getColorNew)
+  })
+  .add('repeat old', () => {
+    repeatForAllNumbers(getColor)
+  })
+  .on('cycle', (event) => {
+    console.log(`${event.target}`)
+  })
+  .on('complete', function () {
+    console.log(`Fastest is ${this.filter('fastest').map('name')}`)
+  })
+  .run()
---
+++ test/map.js
@@ -0,0 +1,31 @@
+function h2rgb (initT) {
+  const t = initT < 0 ? initT + 1 : initT % 1
+
+  if (t < 1 / 6) {
+    return Math.round(t * 1530)
+  }
+  if (t < 1 / 2) {
+    return 255
+  }
+  if (t < 2 / 3) {
+    return Math.round(((2 / 3) - t) * 1530)
+  }
+  return 0
+}
+
+/**
+ * HSL to RGB converter with S = 1 and L = 1
+ * @param {number} hue Number between 0 and 1
+ * @returns {{r: *, b: *, g: *}} Color converted to RGB
+ *
+ * @example
+ * import mapColor from 'number-to-color/map'
+ * const tenthColorOfSixteen = mapColor(10 / 16) // equals {r: 0, g: 64, b: 255}
+ */
+module.exports = function (hue) {
+  return {
+    r: h2rgb(hue + (1 / 3)),
+    g: h2rgb(hue),
+    b: h2rgb(hue - (1 / 3)),
+  }
+}
